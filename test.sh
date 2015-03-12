b8562f744c24ea3dbf48d34239003c98d665f0ab
Christopher Gervais <chris@ergonlogic.com>
2015-03-12 01:43:44 -0400

    Fix tests.

--- test.sh
+++ test.sh
@@ -34,34 +34,34 @@ else
 fi
 
 echo 'Checking NFS mount on host.'
-grep 'nfs_test" 10.234.234.234' /etc/exports | report_result
+grep 'nfs_test" 10.234.234.234' /etc/exports > /dev/null; report_result
 
 echo 'Checking NFS mount on VM.'
-vagrant ssh -c'mount'  -- -q | grep nfs_test | report_result
+vagrant ssh -c'mount'  -- -q | grep nfs_test > /dev/null; report_result
 
 echo 'Creating local test file.'
-echo 'test1234' > nfs_test/local.txt | report_result
+echo 'test1234' > nfs_test/local.txt; report_result
 
 echo 'Checking that local test file exists on VM.'
-vagrant ssh -c'[ -f /tmp/nfs_test/local.txt ]'  -- -q | report_result
+vagrant ssh -c'[ -f /tmp/nfs_test/local.txt ]'  -- -q > /dev/null; report_result
 
 echo 'Checking local test file contents on VM.'
-vagrant ssh -c'cat /tmp/nfs_test/local.txt'  -- -q | grep ^test1234 | report_result
+vagrant ssh -c'cat /tmp/nfs_test/local.txt'  -- -q | grep ^test1234 > /dev/null; report_result
 
 echo 'Checking local test file ownership on VM.'
-vagrant ssh -c'ls -la /tmp/nfs_test/local.txt' -- -q | grep "test_user www-data" | report_result
+vagrant ssh -c'ls -la /tmp/nfs_test/local.txt' -- -q | grep "test_user www-data" > /dev/null; report_result
 
 echo 'Creating remote test file.'
-vagrant ssh -c'echo test4321 > /tmp/nfs_test/remote.txt' -- -q | report_result
+vagrant ssh -c'echo test4321 > /tmp/nfs_test/remote.txt' -- -q > /dev/null; report_result
 
 echo 'Checking that remote test file exists locally.'
-[ -f nfs_test/remote.txt ] | report_result
+[ -f nfs_test/remote.txt ]; report_result
 
 echo 'Checking remote test file contents locally.'
-cat nfs_test/remote.txt | grep ^test4321 | report_result
+cat nfs_test/remote.txt | grep ^test4321 > /dev/null; report_result
 
 echo 'Checking remote test file ownership locally.'
-ls -la nfs_test/remote.txt 2>&1 | grep "${username} ${group}" | report_result
+ls -la nfs_test/remote.txt | grep -E "(${username}|${group})" > /dev/null; report_result
 
 echo -e "\n ${green}** ALL TESTS PASSED **\n"; tput sgr0
 
